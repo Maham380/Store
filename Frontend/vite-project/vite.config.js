8a34c3037f16360ae88695f04fb90e78b407cb39
Mahamarain@gmail.com <mahammarain676@gmail.com>
2025-12-29 13:59:21 +0500

    Fix Vite base path for GitHub Pages

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -12,5 +12,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Store/'  // <-- yeh tumhare repo ka naam
+  base: '/Store/'  // ye repo name ka folder
 })
